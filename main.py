5e332fbe417b3d08fdab4b2c5649ecafd6e2d535
陳彥智 <ychen7157@gmail.com>
2025-10-22 18:40:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import streamlit as st
 
 page = st.navigation([
     st.Page('p1.py',title='IRIS相關資訊', icon='🌸'),
-    st.Page('p2.py',title='單一分類器展示', icon='🌸'),
+    #st.Page('p2.py',title='單一分類器展示', icon='🌸'),
     st.Page('p3.py',title='iris品種預測', icon='🌸')
 ])
+
 
 page.run()
